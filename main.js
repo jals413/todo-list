e483cc4433354180505574960bf19b9fcc39cf0a
Jal Thakor <jalsthakor413@gmail.com>
2023-04-21 17:10:29 +0530

    done

--- main.js
+++ main.js
@@ -1,45 +1,128 @@
 const inputBox = document.getElementById("input-box");
+
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
+function addTask()
+{
     if(inputBox.value === '')
     {
         alert("Please Enter a Task!");
     }
+
     else if(inputBox.value.length > 30)
     {
         alert("Task is too Large");
     }
+
     else
     {
         let li=document.createElement("li");
+
         li.innerHTML= inputBox.value;
+
         listContainer.appendChild(li);
+
         let span = document.createElement("span");
+
         span.innerHTML = "\u00d7";
+
         li.appendChild(span);
     }
+
     inputBox.value="";
+
     saveData();
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
+listContainer.addEventListener("click", function(e)
+{
+    if(e.target.tagName === "LI")
+    {
         e.target.classList.toggle("checked");
+
         saveData();
     }
-    else if(e.target.tagName === "SPAN"){
+    else if(e.target.tagName === "SPAN")
+    {
         e.target.parentElement.remove();
+
         saveData();
     }
 
-},false);
+} , false);
 
-function saveData(){
+function saveData()
+{
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
-function showData(){
+function showData()
+{
     listContainer.innerHTML=localStorage.getItem("data");
 }
+
+// const inputBox = document.getElementById("input-box");
+
+// const listContainer = document.getElementById("list-container");
+
+// function addTask()
+// {
+//     if(inputBox.value === '')
+//     {
+//         alert("Please Enter a Task!");
+//     }
+
+//     else if(inputBox.value.length > 30)
+//     {
+//         alert("Task is too Large");
+//     }
+
+//     else
+//     {
+//         let li=document.createElement("li");
+
+//         li.innerHTML= inputBox.value;
+
+//         listContainer.appendChild(li);
+
+//         let span = document.createElement("span");
+
+//         span.innerHTML = "\u00d7";
+
+//         li.appendChild(span);
+//     }
+
+//     inputBox.value="";
+
+//     saveData();
+// }
+
+// listContainer.addEventListener("click", function(e)
+// {
+//     if(e.target.tagName === "LI")
+//     {
+//         e.target.classList.toggle("checked");
+
+//         saveData();
+//     }
+//     else if(e.target.tagName === "SPAN")
+//     {
+//         e.target.parentElement.remove();
+
+//         saveData();
+//     }
+
+// } , false);
+
+// function saveData()
+// {
+//     localStorage.setItem("data", listContainer.innerHTML);
+// }
+
+// function showData()
+// {
+//     listContainer.innerHTML=localStorage.getItem("data");
+// }
+// showData();
+
 showData();
